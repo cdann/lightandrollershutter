bdeea93bd440906887193989856dc6375d6fe339
Celine <celine.dannappe@gmail.com>
2023-03-26 19:17:07 +0200

    Initial Commit

---
+++ testdevices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testdevices
+//
+//  Created by celine dann on 26/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
